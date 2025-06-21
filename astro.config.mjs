7ce3718947eda9b9fe7db97fcf9899f689c97db8
Gabriel Aedo <aedothegabriel@gmail.com>
2025-06-21 12:01:03 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://GabrielAedoPozo.github.io',
-    base: '/portafolio.', 
+    base: '/portafolio1', 
 });
